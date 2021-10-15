05d48fc1d1129d6801ffdb6ce9aaa976725946a3
Justin Lowry <github@lowry.life>
2021-10-15 09:25:30 -0600

    sldc page finished

--- sldc.js
+++ sldc.js
@@ -6,14 +6,22 @@
 */
 
 // Analysis of Requirements
+// Planning
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
 /*
-    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    Create a variable for each of the above steps of the Software Development Lifecycle and 
+    set the values of each variable to a definition of the step.
 */
+
+let analysisOfRequirements = "Researching and finding what the problems are. How can we improve the system."
+let planning = "What do we want. Things we can afford to put time and money into. What would our customers need"
+let design = "How it will be implemented. What we decided to implement. User flow, program flow."
+let implementation = "Building the project. Working with devs, creators, artist, ect"
+let testingAndIntegration = "Making sure the product is stable. Testing for different cases to ensure a good user experience"
+let maintain = "Updating the app to keep bugs to a minimum. Updating the app to keep it fast and easy and up to current technological standards."
